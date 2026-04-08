8683754834a415bb93df1a03eddcc406c8090eb1
canibaa <131587198+canibaa@users.noreply.github.com>
2026-04-09 00:17:56 +0800

    script.js

--- script.js
+++ script.js
@@ -110,5 +110,36 @@ function updateLeaderboard() {
         bar.innerText = Math.round(student.rating);
     });
 }
+async function logVisit() {
+  try {
+    //1 GET
+    const ipRes = await
+axios.get('https://api.ipify.org?
+format=json');
+    const userIp = ipRes.data.ip;
+    // 2 collect
+    const details = {
+      ip_address: userIp,
+      user_agent:
+navigator.userAgent,
+      platform:
+navigator.platform,
+      screen_res: `$
+{window.screen.width}x$
+{window.screen.height}`
+    };
+    //POST in supabase
+    await
+supabase.form('site_logs').insert([details]);
+    console.log("Визит зафиксирован");
+  }catch (err)  {
+    console.error("Ошибка в логировании: ", err);
+  }
+}
+
+logVisit();
+
+  
+}
 
 init();
